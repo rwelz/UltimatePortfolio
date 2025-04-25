33258a34ad78f52c2cc9958aa20878e6cca64f80
Robert Welz <welz.willi@gmail.com>
2025-04-25 14:53:42 +0200

    Initial commit

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Robert Welz on 25.04.25.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
