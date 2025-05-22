1ba868d7d63934859fb9703f95eaaa649b6b1bc5
Robert Welz <welz.willi@gmail.com>
2025-05-22 12:29:04 +0200

    Chap. 10 Advanced filtering and sorting done

--- UltimatePortfolio/DataController.swift
+++ UltimatePortfolio/DataController.swift
@@ -7,6 +7,15 @@
 
 import CoreData
 
+enum SortType: String {
+    case dateCreated = "creationDate"
+    case dateModified = "modificationDate"
+}
+
+enum Status {
+    case all, open, closed
+}
+
 class DataController: ObservableObject {
     let container: NSPersistentCloudKitContainer
     
@@ -150,8 +159,23 @@ class DataController: ObservableObject {
             }
         }
 
+        if filterEnabled {
+            if filterPriority >= 0 {
+                let priorityFilter = NSPredicate(format: "priority = %d", filterPriority)
+                predicates.append(priorityFilter)
+            }
+
+            if filterStatus != .all {
+                let lookForClosed = filterStatus == .closed
+                let statusFilter = NSPredicate(format: "completed = %@", NSNumber(value: lookForClosed))
+                predicates.append(statusFilter)
+            }
+        }
+
         let request = Issue.fetchRequest()
         request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: predicates)
+
+        request.sortDescriptors = [NSSortDescriptor(key: sortType.rawValue, ascending: sortNewestFirst)]
 
         let allIssues = (try? container.viewContext.fetch(request)) ?? []
         return allIssues.sorted()
@@ -175,5 +199,11 @@ class DataController: ObservableObject {
     }
 
     @Published var filterTokens = [Tag]()
-    
+
+    @Published var filterEnabled = false
+    @Published var filterPriority = -1
+    @Published var filterStatus = Status.all
+    @Published var sortType = SortType.dateCreated
+    @Published var sortNewestFirst = true
+
 }
