b20692a5939d2a46a5e2dc2bf055f2dfb40d2378
Robert Welz <welz.willi@gmail.com>
2025-06-19 15:48:09 +0200

    Chap. 29 done

---
+++ UltimatePortfolio/Filters/SidebarViewModel.swift
@@ -0,0 +1,87 @@
+//
+//  SidebarViewModel.swift
+//  UltimatePortfolio
+//
+//  Created by Robert Welz on 19.06.25.
+//
+
+import CoreData
+import Foundation
+
+extension SidebarView {
+    class ViewModel: NSObject, ObservableObject, NSFetchedResultsControllerDelegate {
+        var dataController: DataController
+
+        // @FetchRequest funktioniert nur innerhalb von Views, deswegen wird es ersetzt:
+        // @FetchRequest(sortDescriptors: [SortDescriptor(\.name)]) var tags: FetchedResults<Tag>
+
+        private let tagsController: NSFetchedResultsController<Tag>
+        @Published var tags = [Tag]()
+
+        @Published var tagToRename: Tag?
+        @Published var renamingTag = false
+        @Published var tagName = ""
+
+        var tagFilters: [Filter] {
+            tags.map { tag in
+                Filter(id: tag.tagID, name: tag.tagName, icon: "tag", tag: tag)
+            }
+        }
+
+        init(dataController: DataController) {
+            self.dataController = dataController
+
+            let request = Tag.fetchRequest()
+            request.sortDescriptors = [NSSortDescriptor(keyPath: \Tag.name, ascending: true)]
+
+            tagsController = NSFetchedResultsController(
+                fetchRequest: request,
+                managedObjectContext: dataController.container.viewContext,
+                sectionNameKeyPath: nil,
+                cacheName: nil
+            )
+
+            super.init()
+
+            tagsController.delegate = self
+
+            // now we finish the new initializer by executing the fetch request and assigning it to the tags property:
+            do {
+                try tagsController.performFetch()
+                tags = tagsController.fetchedObjects ?? []
+            } catch {
+                print("Failed to fetch tags")
+            }
+        }
+
+        func controllerDidChangeContent(_ controller: NSFetchedResultsController<any NSFetchRequestResult>) {
+            if let newTags = controller.fetchedObjects as? [Tag] {
+                    tags = newTags
+                }
+        }
+
+        func delete(_ offsets: IndexSet) {
+            for offset in offsets {
+                let item = tags[offset]
+                dataController.delete(item)
+            }
+        }
+
+        func delete(_ filter: Filter) {
+            guard let tag = filter.tag else { return }
+            dataController.delete(tag)
+            dataController.save()
+        }
+
+        func rename(_ filter: Filter) {
+            tagToRename = filter.tag
+            tagName = filter.name
+            renamingTag = true
+        }
+
+        func completeRename() {
+            tagToRename?.name = tagName
+            dataController.save()
+        }
+    }
+}
