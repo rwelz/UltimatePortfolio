bf690136db419d3c48117c968af72459bea9e614
Robert Welz <welz.willi@gmail.com>
2025-06-20 12:22:08 +0200

    Chap. 30 done

---
+++ UltimatePortfolio/Issues/IssueRowViewModel.swift
@@ -0,0 +1,45 @@
+//
+//  IssueRowViewModel.swift
+//  UltimatePortfolio
+//
+//  Created by Robert Welz on 20.06.25.
+//
+
+import Foundation
+
+extension IssueRow {
+    @dynamicMemberLookup
+    class ViewModel: ObservableObject {
+        let issue: Issue
+
+        var iconOpacity: Double {
+            issue.priority == 2 ? 1 : 0
+        }
+
+        var iconIdentifier: String {
+            issue.priority == 2 ? "\(issue.issueTitle) High Priority" : ""
+        }
+
+        var accessibilityHint: String {
+            issue.priority == 2 ? "High priority" : ""
+        }
+
+        var accessibilityCreationDate: String {
+            issue.issueCreationDate.formatted(date: .abbreviated, time: .omitted)
+        }
+
+        var creationDate: String {
+            issue.issueCreationDate.formatted(date: .numeric, time: .omitted)
+            }
+
+        init(issue: Issue) {
+            self.issue = issue
+        }
+
+
+        subscript<Value>(dynamicMember keyPath: KeyPath<Issue, Value>) -> Value {
+            issue[keyPath: keyPath]
+        }
+        // now with this subscript and @dynamicMemberLookup we can write in IssueRow.swift: viewModel.issueTitle instead of viewModel.issue.issueTitle
+    }
+}
