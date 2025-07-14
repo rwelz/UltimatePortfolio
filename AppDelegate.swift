519676026a9c37299b5302b39ec9d18c4eef2f6e
Robert Welz <welz.willi@gmail.com>
2025-07-14 16:44:08 +0200

    Chap. 38 done

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -5,4 +5,18 @@
 //  Created by Robert Welz on 14.07.25.
 //
 
-import Foundation
+import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        let sceneConfiguration = UISceneConfiguration(name: "Default", sessionRole: connectingSceneSession.role)
+        sceneConfiguration.delegateClass = SceneDelegate.self
+        return sceneConfiguration
+    }
+}
+
+
